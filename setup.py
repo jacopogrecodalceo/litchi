09d5bed6cb7b07aa81554cdedb9c3971e04ad5d2
jacopogrecodalceo <jacopo.grecodalceo@gmail.com>
2025-01-17 16:01:40 +0100

    version updated

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='litchi',
-    version='0.1.0',
+    version='0.1.1',
     description='A brief description of your library',
     author="jacopo greco d'alceo",
     author_email='your_email@example.com',
